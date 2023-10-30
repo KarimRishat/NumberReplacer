b68a5055a4d1cbbc8c288b93308a29d64e93f32a
Ришат Каримов <karimr2000@mail.ru>
2023-10-30 21:59:06 +0300

    Добавьте файлы проекта.

---
+++ TestTaskFlexibase/NumberReplacer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskFlexibase
+{
+    internal class NumberReplacer
+    {
+        private List<Strategy> strategyList;
+        public string NewString(int num)
+        {
+            string result = "";
+            foreach (Strategy strategy in strategyList)
+            {
+                if (strategy.NeedReplace(num))
+                {
+                    result.Insert(result.Length - 1, strategy.Replace());
+                    result.Insert(result.Length - 1, "-");
+                }
+            }
+            return result.Substring(0,result.Length-1);
+        }
+    }
+}
---
+++ TestTaskFlexibase/Strategy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskFlexibase
+{
+    internal class Strategy
+    {
+        private string replace_string;
+        private uint Divider;
+        public Strategy(string text, int divider) { 
+            replace_string = text;
+            if(divider > 0) { 
+                Divider = (uint)divider; 
+            }
+
+            else {
+                throw new ArgumentOutOfRangeException("Делитель должен быть больше нуля");
+            }
+        }
+        public bool NeedReplace(int number)
+        {
+            bool fizz = false;
+            if (number % Divider == 0)
+                fizz = true;
+            return fizz;
+        }
+
+        public string Replace()
+        {
+            return replace_string;
+        }
+    }
+}
